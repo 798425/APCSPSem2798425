49593a2377f7b55a8af053364b8c988241029a94
798425 <54591206+798425@users.noreply.github.com>
2020-01-29 08:50:36 -0800

    boid

--- lab 128/boids.js
+++ lab 128/boids.js
@@ -5,4 +5,29 @@ class Boids{
     this.acc = createVector(dx, dy);
     this.clr = color(255, 0, 0);
   }
+  run(){
+    this.update();
+    this.checkEdges();
+    this.render();
+  }
+  update(){
+
+  }
+  checkEdges(){
+    if (this.loc.x < 0){
+      this.vel.x = -(this.vel.x);
+    }
+    if (this.loc.x > 800){
+      this.vel.x = -(this.vel.x);
+    }
+    if (this.loc.y < 0){
+      this.vel.y = -(this.vel.y);
+    }
+    if (this.loc.y > 800){
+      this.vel.y = -(this.vel.y);
+    }
+  }
+  render(){
+
+  }
 }
--- lab 128/sketch.js
+++ lab 128/sketch.js
@@ -2,6 +2,7 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
+var boids = []
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
@@ -9,4 +10,18 @@ function setup() {
   fill(200, 30, 150);
 
 
+}
+function draw(){
+  runBoids();
+}
+function loadBoids(){
+  for var (i = 0; i < ){
+
+  }
+}
+
+function runBoids(){
+  for(var i = 0; i < boids.length; i++){
+    boids[i].run();
+  }
 }
