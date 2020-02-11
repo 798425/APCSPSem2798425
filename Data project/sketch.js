f334f15852263993aaf298ae7e3effbbb4546a15
798425 <54591206+798425@users.noreply.github.com>
2020-02-11 09:49:39 -0800

    nothing todai

--- Data project/sketch.js
+++ Data project/sketch.js
@@ -3,15 +3,20 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 var stats;
+var statsArray = [];
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   loadPlayerStats("Kobe Bryant");
+  console.log("kobe bryant");
+  console.log(aggregateStats("Kobe Bryant", ));
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
+  createPlayerSelectionList();
+  getSelectedPlayers();
 }
 
 function loadPlayerStats("Kobe Bryant") {
@@ -25,7 +30,6 @@ playerSel = createSelect(true);
 playerSel.position((windowWidth-width)/2 + 270, (windowHeight-height)/2 + 40); // locate at 270,40 in canvas coordinates playerSel.size(150,headerHeight-50);
 }
 
-
   // locate at 270,40 in canvas coordinates playerSel.size(150,headerHeight-50);
 
   function getSelectedPlayers() {
@@ -34,7 +38,8 @@ playerSel.position((windowWidth-width)/2 + 270, (windowHeight-height)/2 + 40); /
       chosenPlayers.push(playerSel.elt.selectedOptions[i].value);
   }
 }
-function aggregateStats(player, stat) {
+
+function aggregateStats("Kobe Bryant", height) {
   results = [];
   for (var i =0; i<statsArray.length; i++) {
 
