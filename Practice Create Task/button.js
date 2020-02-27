894331c727bb8e391d84691ec5e85a5eedbfe37e
798425 <54591206+798425@users.noreply.github.com>
2020-02-27 09:49:16 -0800

    gggggg

---
+++ Practice Create Task/button.js
@@ -0,0 +1,68 @@
+class Button{
+  constructor(x, y, w, h, msg, clr, id){
+    this.loc = createVector(x,y);
+    this.h = h;
+    this.w = w;
+    this.msg = msg;
+    this.clr = clr;
+    this.id = id;
+  }
+  //actually creating buttons
+  run(){
+    this.render();
+    this.isPressed();
+  }
+  //drawing buttons
+  render(){
+    fill(this.clr);
+    rect(this.loc.x, this.loc.y, this.w, this.h, 20);
+    fill(10);
+    textSize(20);
+    if (this.id === 4){
+      textSize(18);
+      text(this.msg, this.loc.x, this.loc.y + 30);
+    }else{
+      textSize(20);
+      text(this.msg, this.loc.x + 10, this.loc.y + 30);
+    }
+  }
+  //checks if the button is pressed and makes different buttons do different things
+  isPressed(){
+    if (mouseX > this.loc.x && mouseX < this.loc.x + this.w && mouseY > this.loc.y && mouseY < this.loc.y + this.h && mouseIsPressed){
+      if(this.id == 1){
+        gameStat = 2;
+        difficulty = 5;
+        health = 5;
+        score = 0;
+        winScore = 10;
+        loadStuff(difficulty);
+      }
+      if(this.id == 2){
+        gameStat = 2;
+        difficulty = 10;
+        health = 5;
+        score = 0;
+        winScore = 15;
+        loadStuff(difficulty);
+      }
+      if(this.id == 3){
+        gameStat = 2;
+        difficulty = 15;
+        health = 3;
+        score = 0;
+        winScore = 20;
+        loadStuff(difficulty);
+      }
+      if(this.id == 4){
+        fill(255);
+        text("Click on a difficulty and try to catch the balls on the top of the paddle.", 90, 650);
+        text("If a ball hits the bottom of your paddle, you will lose health.", 90, 670);
+        text("If you catch a certain amount balls before running out of health, you win!", 90, 690);
+      }
+      if(this.id == 5){
+        gameStat = 1;
+        balls = [];
+      }
+    }
+  }
+}
--- Practice Create Task/sketch.js
+++ Practice Create Task/sketch.js
@@ -11,6 +11,14 @@ var w = 20;
 var attractor, repeller;
 var bars = [];
 var numBars, barWidth;
+var balls = [];
+var paddle;
+var gameStat = 1;
+var difficulty, health, score;
+var win = 2;
+var btnEasy, btnMedium, btnHard, btnInstructions;
+var winScore;
+
 
 
 function setup() {
@@ -19,16 +27,115 @@ function setup() {
   background(5, 5, 5);
 loadObjects(50);
 loadSnake();
+loadStuff();
 }
+function loadStuff(n){
+  for (var i = 0; i < n; i++){
+    balls[i] = new Ball(random(width), random(0,200), random(-3,3), random(-3,3));
+  }
+  paddle = new Paddle(width/2, 700);
+}
+function startGam(){
+  win = 2;
+  runButtons();
+  background(200,70,100);
+  runButtons();
+  textSize(90);
+  fill(20,20,20);
+  textFont('Georgia')
+  text("Paddle Game",150,200);
+  fill(random(0,255), random(0,255), random(0,255));
+  ellipse(340, 300, 100, 100);
+  fill(random(0,255), random(0,255), random(0,255));
+  ellipse(460, 300, 100, 100);
+  fill(random(0,255), random(0,255), random(0,255));
+  arc(400, 400, 80, 80, 0, PI, CHORD);
+}
+function endGame(){
+  fill(255);
+  textFont('Times New Roman');
+  textSize(90);
+  if (win === 1){
+    background(10, 200, 100);
+    text("You won", 200, 200);
+    btnMenu = new Button(250, 600, 300, 100, "Menu", color(10,255,10), 5);
+    btnMenu.run();
+    //smiley face
+    fill(random(0,255), random(0,255), random(0,255));
+    ellipse(340, 300, 100, 100);
+    fill(random(0,255), random(0,255), random(0,255));
+    ellipse(460, 300, 100, 100);
+    fill(random(0,255), random(0,255), random(0,255));
+    arc(400, 400, 80, 80, 0, PI, CHORD);
+    }else{
+      background(255, 20, 10);
+      text("Game Over", 150, 200);
+      btnMenu = new Button(250, 600, 300, 100, "Menu", color(10,255,10), 5);
+      btnMenu.run();
+      //frowny face
+      fill(random(0,255), random(0,255), random(0,255));
+      ellipse(340, 300, 100, 100);
+      fill(random(0,255), random(0,255), random(0,255));
+      ellipse(460, 300, 100, 100);
+      fill(random(0,255), random(0,255), random(0,255));
+      arc(400, 400, 80, 80, PI, 0, CHORD);
 
+    }
+  }
+  function makeButtons(){
+    btnEasy = new Button(50, 500, 100, 75, "Easy", color(1, 255, 1), 1);
+    btnMedium = new Button(250, 500, 100, 75, "Medium", color(255, 255, 1), 2);
+    btnHard = new Button(450, 500, 100, 75, "Hard", color(255, 1, 1), 3);
+    btnInstructions = new Button(650, 500, 100, 75, "Instructions",color(0, 255, 255), 4);
+  }
+  //shows buttons
+  function runButtons(){
+    btnEasy.run();
+    btnMedium.run();
+    btnHard.run();
+    btnInstructions.run();
+  }
+  //game code
+  function playGame(){
+    background(128, 128, 128, 50);
+    runPaddle();
+    removeBall();
+    if (health <= 0){
+      win = 0;
+      gameState = 3;
+    }
+    if (win === 1){
+      gameState = 3;
+    }
+  }
+  function runPaddle(){
+    for(var t = 0; t < pp.length; t++){
+      pp[t].run();
+    }
+    fill(255);
+    textSize(20);
+    text("Score = "+ score, 20, 50);
+    text("Health = "+ health, 700, 50);
+    paddle.run();
+  }
 //  The draw function is called @ 30 fps
 function draw() {
   background(0,0,0);
+  if (gameStat === 1){
+    startGame();
+  }
+  if (gameStat === 2){
+    playGame();
+  }
+  if (gameStat === 3){
+    endGame();
+  }
+
 if(gameState === 1){
   startGame();
 }
 if(gameState === 2){
-  playGame();
+  playSnake();
 }
 if(gameState === 3){
   textSize(45);
@@ -92,10 +199,18 @@ function startGame(){
     gameState = 2;
   }
 }
-function playGame(){
+function playSnake(){
   runSnake(); //runs snake head
   textSize(20);
   text("score = " + score, 10, 20);
 
   // background(0,0,0);
  }
+ function removeBall(){
+  for (var i = balls.length - 1; i >= 0; i--){
+    if (balls[i].isColliding()){
+      balls.splice(i,1);
+      health = health -1;
+    }
+  }
+}
