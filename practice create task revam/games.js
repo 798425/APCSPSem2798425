0843f6124855f5e477e76a990547aefa241e46d9
798425 <54591206+798425@users.noreply.github.com>
2020-03-05 09:49:35 -0800

    dsk fj

--- practice create task revam/games.js
+++ practice create task revam/games.js
@@ -1,17 +1,27 @@
 class Games{
-  constructor(x, y, w, h, clr){
+  constructor(x, y, w, h, clr, id){
   this.loc = createVector(x, y);
   this.h = h;
   this.w = w;
   this.clr = color(random(0, 255), random(0, 255), random(0, 255));
+  this.id = id;
   }
   run(){
-    render();
-    update();
+    this.render();
+    this.isPressed()
   }
   render(){
     fill(this.clr);
-    rect(200, 500, 200, 300);
-    
+    rect(this.loc.x, this.loc.y, this.w, this.h);
+    if(this.id === 0){
+      textSize(15);
+      text("Snake Game", 100, 500);
+
+    }else{
+      textSize(15);
+      text("Paddle Game",600, 500)
+
+    }
+
   }
 }
